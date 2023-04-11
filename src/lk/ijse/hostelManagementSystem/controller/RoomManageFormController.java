3c43f0a41df9cf57aa1948fd590e9f5e8011b061
vimukthinirmana <vimukthinirmana64@gmail.com>
2023-04-11 20:58:07 +0530

    update

--- src/lk/ijse/hostelManagementSystem/controller/RoomManageFormController.java
+++ src/lk/ijse/hostelManagementSystem/controller/RoomManageFormController.java
@@ -1,4 +1,36 @@
 package lk.ijse.hostelManagementSystem.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class RoomManageFormController {
+    public Label lblRoomID;
+    public ComboBox cbxRoomType;
+    public TextField txtQty;
+    public TextField txtKeyMoney;
+
+
+    public TableView roomTableView;
+    public TableColumn colRoomID;
+    public TableColumn colRooType;
+    public TableColumn colKeyMoney;
+    public TableColumn colQty;
+
+    public void cbxRoomTypeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void saveBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void clearBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void addRoomBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void canselBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteBtnOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/lk/ijse/hostelManagementSystem/controller/StudentManageFormController.java
+++ src/lk/ijse/hostelManagementSystem/controller/StudentManageFormController.java
@@ -1,4 +1,39 @@
 package lk.ijse.hostelManagementSystem.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class StudentManageFormController {
+
+    public Label lblStudentID;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtAge;
+    public TextField txtContactNo;
+    public ComboBox cbxGender;
+    public TableView studentTableView;
+    public TableColumn colStudentID;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colGender;
+    public TableColumn colAge;
+    public TableColumn colContactNo;
+
+    public void saveBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void clearBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void cbxGenderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void addStudentBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void canselBtnOnAction(ActionEvent actionEvent) {
+    }
 }
